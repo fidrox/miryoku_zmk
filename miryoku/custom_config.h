bea8d639b4c56a2fcaedb631ea77744948a2ffed
fidrox <fidrox@gmail.com>
2023-08-25 12:06:01 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,10 +9,10 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, RET),U_LT(U_SYM, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_SWITCH, RET),U_LT(U_SYM, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
-#define MIRYOKU_LAYER_MOUSE \
-&u_to_U_REM,       &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+#define MIRYOKU_LAYER_SWITCH \
+&u_to_U_REM,       &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &kp D,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_NU,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
 U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_NU,              U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
@@ -46,11 +46,13 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(REM,   "Remnant")
+MIRYOKU_X(REM,   "Remnant") \
+MIRYOKU_X(SWITCH,   "Switch")
 
 
 
 #define MIRYOKU_LAYERMAPPING_REM MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_SWITCH MIRYOKU_MAPPING
 
 
 
@@ -65,4 +67,5 @@ MIRYOKU_X(REM,   "Remnant")
 #define U_SYM    8
 #define U_FUN    9
 #define U_REM   10
+#define U_REM   11
 
