a029eb1362533b74f1e8e2cabe83dc9b2571517e
fidrox <fidrox@gmail.com>
2023-08-27 21:59:08 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -15,7 +15,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U
 &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 &kp TAB,           &kp Z,             &kp X,             &kp C,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&kp M,&kp SPACE,U_LT(U_MOUSE, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),&kp M,&kp SPACE,U_LT(U_MOUSE, SPACE),  U_LT(U_NUM, BSPC), &u_to_U_BASE,  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SWITCH \
 &u_to_U_REM,       &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      &kp D,             &u_to_U_REM,       U_NU,              U_CPY,             U_CUT,             U_UND,             \
